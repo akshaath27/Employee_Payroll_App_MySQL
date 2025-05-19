107e65ad742aea2bec26be49a0c4c4360255a587
Akshaath <nakshaath@gmail.com>
2025-05-19 17:31:40 +0530

    UC2: Added: REST Controller with MySQL Application Properties

--- src/main/java/com/example/employeepayrollapp/controller/EmployeePayrollController.java
+++ src/main/java/com/example/employeepayrollapp/controller/EmployeePayrollController.java
@@ -2,12 +2,45 @@ package com.example.employeepayrollapp.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import com.example.employeepayrollapp.dto.EmployeeDTO;
+import com.example.employeepayrollapp.model.Employee;
+import com.example.employeepayrollapp.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
 
 @RestController
-public class EmployeePayrollController {
+@RequestMapping("/employee")
+@CrossOrigin(origins = "*")
+public class EmployeeController {
+	@Autowired
+    private EmployeeService service;
 
-    @GetMapping("/")
-    public String welcomeMessage() {
-        return "Welcome to Spring Boot Employee Payroll App using MySQL";
+    @PostMapping
+    public Employee create(@RequestBody EmployeeDTO dto) {
+        return service.addEmployee(dto);
     }
+
+    @GetMapping
+    public List<Employee> getAll() {
+        return service.getAllEmployees();
+    }
+
+    @GetMapping("/{id}")
+    public Employee getById(@PathVariable Long id) {
+        return service.getEmployeeById(id);
+    }
+
+    @PutMapping("/{id}")
+    public Employee update(@PathVariable Long id, @RequestBody EmployeeDTO dto) {
+        return service.updateEmployee(id, dto);
+    }
+
+    @DeleteMapping("/{id}")
+    public String delete(@PathVariable Long id) {
+        service.deleteEmployee(id);
+        return "Deleted employee with id: " + id;
+    }
+
 }
